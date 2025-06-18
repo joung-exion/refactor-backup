0e30d10362227792b190a8c4b57c50fcc0ea6551
joung-exion <joung-exion@users.noreply.github.com>
2025-06-18 17:09:41 +0000

    Fix type errors, start bandcamp api module

--- internal/app/app.go
+++ internal/app/app.go
@@ -44,3 +44,5 @@ puts "Project #{project.name} has #{project.files.size} file(s)"
 # Additional Implementation 1760681495
 
 # Additional Implementation 1760681495
+
+# Additional Implementation 1760681495
